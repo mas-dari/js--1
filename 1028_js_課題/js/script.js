3fb4e397601979a194293a20316692d36aa2561e
masdari <masdari.tech@gmail.com>
2023-11-02 16:20:20 +0900

    画像修正

--- 1028_js_課題/js/script.js
+++ 1028_js_課題/js/script.js
@@ -24,13 +24,13 @@ $(".rock").on("click", function () {
 // グーを出した時の勝敗を表示
     if (cpchoice === 0) {
         console.log("0"); 
-        $(".result").html('<img src="/Users/ryoma.masuda/Desktop/G/1021_js_課題/img/movie_couple.png" alt="あいこの画像">');
+        $(".result").html('<img src="img/movie_couple.png" alt="あいこの画像">');
     }else if(cpchoice === 1){
         console.log("1");
-        $(".result").html('<img src="/Users/ryoma.masuda/Desktop/G/1021_js_課題/img/pose_win_boy.png" alt="勝ちの画像">');
+        $(".result").html('<img src="img/pose_win_boy.png" alt="勝ちの画像">');
     }else if(cpchoice === 2){
         console.log("2");
-        $(".result").html('<img src="/Users/ryoma.masuda/Desktop/G/1021_js_課題/img/pose_lose_boy.png" alt="負けの画像">');
+        $(".result").html('<img src="img/pose_lose_boy.png" alt="負けの画像">');
     }    
 });
 
@@ -43,25 +43,25 @@ $(".scissors").on("click", function () {
     
     if (cpchoice === 0) {
         console.log("0"); 
-        $(".cp").html('<img src="/Users/ryoma.masuda/Desktop/G/1021_js_課題/img/rock.png" alt="グーの画像">');
+        $(".cp").html('<img src="img/rock.png"" alt="グーの画像">');
     }else if(cpchoice === 1){
         console.log("1");
-        $(".cp").html('<img src="/Users/ryoma.masuda/Desktop/G/1021_js_課題/img/scissors.png" alt="チョキの画像">');
+        $(".cp").html('<img src="img/scissors.png" alt="チョキの画像">');
     }else if(cpchoice === 2){
         console.log("2");
-        $(".cp").html('<img src="/Users/ryoma.masuda/Desktop/G/1021_js_課題/img/paper.png" alt="パーの画像">');
-    }    
+        $(".cp").html('<img src="img/paper.png" alt="パーの画像">');
+    }  
   
 // チョキを出した時の勝敗を表示
     if (cpchoice === 0) {
         console.log("0"); 
-        $(".result").html('<img src="/Users/ryoma.masuda/Desktop/G/1021_js_課題/img/pose_lose_boy.png" alt="負けの画像">');
+        $(".result").html('<img src="img/pose_lose_boy.png" alt="負けの画像">');
     }else if(cpchoice === 1){
         console.log("1");
-        $(".result").html('<img src="/Users/ryoma.masuda/Desktop/G/1021_js_課題/img/movie_couple.png" alt="あいこの画像">');
+        $(".result").html('<img src="img/movie_couple.png" alt="あいこの画像">');
     }else if(cpchoice === 2){
         console.log("2");
-        $(".result").html('<img src="/Users/ryoma.masuda/Desktop/G/1021_js_課題/img/pose_win_boy.png" alt="勝ちの画像">');
+        $(".result").html('<img src="img/pose_win_boy.png" alt="勝ちの画像">');
     }    
 });
 
@@ -75,25 +75,26 @@ $(".paper").on("click", function () {
     
     if (cpchoice === 0) {
         console.log("0"); 
-        $(".cp").html('<img src="/Users/ryoma.masuda/Desktop/G/1021_js_課題/img/rock.png" alt="グーの画像">');
+        $(".cp").html('<img src="img/rock.png"" alt="グーの画像">');
     }else if(cpchoice === 1){
         console.log("1");
-        $(".cp").html('<img src="/Users/ryoma.masuda/Desktop/G/1021_js_課題/img/scissors.png" alt="チョキの画像">');
+        $(".cp").html('<img src="img/scissors.png" alt="チョキの画像">');
     }else if(cpchoice === 2){
         console.log("2");
-        $(".cp").html('<img src="/Users/ryoma.masuda/Desktop/G/1021_js_課題/img/paper.png" alt="パーの画像">');
-    }    
+        $(".cp").html('<img src="img/paper.png" alt="パーの画像">');
+    } 
+  
   
 // パーを出した時の勝敗を表示
     if (cpchoice === 0) {
         console.log("0"); 
-        $(".result").html('<img src="/Users/ryoma.masuda/Desktop/G/1021_js_課題/img/pose_win_boy.png" alt="勝ちの画像">');
+        $(".result").html('<img src="img/pose_win_boy.png" alt="勝ちの画像">');
     }else if(cpchoice === 1){
         console.log("1");
-        $(".result").html('<img src="/Users/ryoma.masuda/Desktop/G/1021_js_課題/img/pose_lose_boy.png" alt="負けの画像">');
+        $(".result").html('<img src="img/pose_lose_boy.png" alt="負けの画像">');
     }else if(cpchoice === 2){
         console.log("2");
-        $(".result").html('<img src="/Users/ryoma.masuda/Desktop/G/1021_js_課題/img/movie_couple.png" alt="あいこの画像">');
+        $(".result").html('<img src="img/movie_couple.png" alt="あいこの画像">');
     }    
 });
 
@@ -101,15 +102,15 @@ $(".paper").on("click", function () {
 //   自分が出した手を表示
 
 $(".rock").on("click", function () {
-    $(".player").html('<img src="/Users/ryoma.masuda/Desktop/G/1021_js_課題/img/rock.png" alt="グーの画像">');
+    $(".player").html('<img src="img/rock.png" alt="グーの画像">');
 });
 
 $(".scissors").on("click", function () {
-    $(".player").html('<img src="/Users/ryoma.masuda/Desktop/G/1021_js_課題/img/scissors.png" alt="チョキの画像">');
+    $(".player").html('<img src="img/scissors.png" alt="チョキの画像">');
 });
 
 $(".paper").on("click", function () {
-    $(".player").html('<img src="/Users/ryoma.masuda/Desktop/G/1021_js_課題/img/paper.png" alt="パーの画像">');
+    $(".player").html('<img src="img/paper.png" alt="パーの画像">');
 });
 
 
